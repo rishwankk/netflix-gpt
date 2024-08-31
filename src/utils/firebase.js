f7254b8415b4acdd0ded8228411f08683b1c9082
rishwankk <rishwan063@gmail.com>
2024-08-31 18:18:00 +0530

    TMDB data fetch && built in main container

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API,
-  authDomain: "netflix-gpt-a4bb4.firebaseapp.com",
-  projectId: "netflix-gpt-a4bb4",
-  storageBucket: "netflix-gpt-a4bb4.appspot.com",
-  messagingSenderId: "449323715985",
-  appId: "1:449323715985:web:a6071b5eef3a0f2b6b9e30",
-  measurementId: "G-JLCB0L2F6X"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
